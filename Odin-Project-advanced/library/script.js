57e39e0d63a9d4cb492e4c4a8eeaf3e4c0fc7780
Mike7Dre <michael.drebant@gmail.com>
2024-05-12 21:28:01 +0200

    html structure

---
+++ Odin-Project-advanced/library/script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+const New = document.querySelector(".new");
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+
+New.addEventListener("click", () => {
+    console.log("test")
+})
