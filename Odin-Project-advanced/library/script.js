43b47362a4b5662b5158558ad9d5035b3289fc9a
Mike7Dre <michael.drebant@gmail.com>
2024-05-13 19:49:46 +0200

    finish array next

--- Odin-Project-advanced/library/script.js
+++ Odin-Project-advanced/library/script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 const New = document.querySelector(".new");
 
+
 function Book() {
 
 }
@@ -10,6 +11,60 @@ function addBookToLibrary() {
 }
 
 
-New.addEventListener("click", () => {
-    console.log("test")
+New.addEventListener("click", () => { 
+  const previousForm = document.querySelector("#myForm");
+  if (previousForm) {
+    previousForm.remove();
+  }
+
+  const form = document.createElement("form");
+  form.id = "myForm";
+
+  const name = document.createElement("input");
+  name.type = "text";
+  name.placeholder = "Who is the author?";
+  form.appendChild(name);
+
+  const title = document.createElement("input");
+  title.type = "text";
+  title.placeholder = "What is the book called?";
+  form.appendChild(title);
+
+  const pages = document.createElement("input");
+  pages.type = "text";
+  pages.placeholder = "How many pages?";
+  form.appendChild(pages);
+
+  const read = document.createElement("input");
+  read.type = "text";
+  read.placeholder = "Have you read it?";
+  form.appendChild(read);
+
+  // Create input for submit button
+  const submitButton = document.createElement('input');
+  submitButton.type = 'submit';
+  submitButton.value = 'Submit';
+
+// Append the submit button to the form
+  form.appendChild(submitButton);
+
+  document.body.appendChild(form);
+
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+
+    const nameData = name.value;
+    const titleData = title.value;
+    const pagesData = pages.value;
+    const readData = read.value;
+
+    console.log(nameData)
+    console.log(titleData)
+    console.log(pagesData)
+    console.log(readData)
+
+
+  })
 })
