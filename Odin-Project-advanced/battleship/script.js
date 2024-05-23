5a06b807a6b646354b7ff5402f239f146cde58b4
Mike7Dre <michael.drebant@gmail.com>
2024-05-23 23:01:06 +0200

    feat Battleship

--- Odin-Project-advanced/battleship/script.js
+++ Odin-Project-advanced/battleship/script.js
@@ -1,5 +1,5 @@
 class Ship {
-  constructor(length, hits, sunk) {
+  constructor(length) {
     this.length = length;
     this.hits = 0;
     this.sunk = false;
@@ -15,3 +15,26 @@ class Ship {
     }
   }
 }
+
+class Gameboard {
+  constructor() {
+    this.board = [];
+    this.missedAttacks = [];
+    this.ships = [];
+  }
+
+  placeShip(length, coordinates) {
+    const newShip = new Ship(length);
+    this.board.push({ ship: newShip, coordinates });
+    this.ships.push(newShip);
+    console.log(newShip);
+    console.log(this.board);
+    return newShip;
+  }
+}
+
+const gameboard = new Gameboard();
+
+const placedShip = gameboard.placeShip(1, [3, 5]);
+console.log(placedShip);
+console.log(gameboard.board);
