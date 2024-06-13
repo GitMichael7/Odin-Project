3238f03139c2eda21991fff3cae08085d81e3df5
Mike7Dre <michael.drebant@gmail.com>
2024-06-13 16:40:33 +0200

    feat shopping

---
+++ Odin-Project-advanced/shopping/src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./navbar.module.css"
+
+const Nav = () => {
+
+    return (
+
+        <>
+        <h2>Random Store</h2>
+        <a href="" className={styles.link}>Home</a>
+        <a href="">Shopping</a>
+        <a href="">Cart</a>
+        </>
+    )
+}
+
+export default Nav
---
+++ Odin-Project-advanced/shopping/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../components/navbar/navbar";
+
+const Home = () => {
+    return (
+        <Nav></Nav>
+        
+    )
+}
+
+export default Home
