13f49140d2d8c71a03f663df605f94ced186015c
Mike7Dre <michael.drebant@gmail.com>
2024-06-15 18:59:53 +0200

    feat BasicSite

--- Odin-Project-advanced/cv/src/App.jsx
+++ Odin-Project-advanced/cv/src/App.jsx
@@ -3,7 +3,10 @@ import Cvform from "./components/form"
 import './App.css'
 
 function App() {
-
+  fetch(
+    "https://api.giphy.com/v1/gifs/random?api_key=r9xEpbboR5c6f2H2dP8iF8iY2yFvrfCP&s=cats",
+    { mode: "cors" }
+  )
   
 
   return (
