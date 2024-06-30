27fba5207d109e1b246956c4eda600c0f4259aa0
Mike7Dre <michael.drebant@gmail.com>
2024-06-30 16:24:05 +0200

    testing tailwind

--- TailwindCSS/my-next-app/src/app/page.js
+++ TailwindCSS/my-next-app/src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-bold underline-offset-8">Hello world!</h1>
       <h1 className="bottom-0 px-4"> Test</h1>
+      <h1 className="bottom-3 px-4"> Test</h1>
     </>
   );
 }
